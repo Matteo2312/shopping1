1332041678b3461de35dd775b3a5cf1f75e74027
matteo lusardi <matteo.lusardi@edu.devinci.fr>
2024-12-21 15:51:03 +0100

    load_data done

--- shopping.py
+++ shopping.py
@@ -59,7 +59,42 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    #The goal is to link each month to its value
+    month_mapping = {
+        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "June": 5,
+        "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
+    }
+
+    evidence = []
+    labels = []
+
+    with open(filename, mode='r') as file:  #We open shopping.csv
+        reader = csv.DictReader(file)
+
+        for row in reader: #we add each row to the corresponding list
+            evidence.append([
+                int(row["Administrative"]),
+                float(row["Administrative_Duration"]),
+                int(row["Informational"]),
+                float(row["Informational_Duration"]),
+                int(row["ProductRelated"]),
+                float(row["ProductRelated_Duration"]),
+                float(row["BounceRates"]),
+                float(row["ExitRates"]),
+                float(row["PageValues"]),
+                float(row["SpecialDay"]),
+                month_mapping[row["Month"]],
+                int(row["OperatingSystems"]),
+                int(row["Browser"]),
+                int(row["Region"]),
+                int(row["TrafficType"]),
+                1 if row["VisitorType"] == "Returning_Visitor" else 0,
+                1 if row["Weekend"] == "TRUE" else 0
+            ])
+            labels.append(1 if row["Revenue"] == "TRUE" else 0)
+
+    return evidence, labels
+
 
 
 def train_model(evidence, labels):
